a144d1dcd2db827c820c15d609f93dd5a10087da
Asen Nikolaev <asen.nikolaev95@abv.bg>
2022-05-02 16:21:49 +0300

    composite pattern - hierarchy of employees

---
+++ src/test/java/design/pattern/structural/composite/EmployeeTest.java
@@ -0,0 +1,26 @@
+package design.pattern.structural.composite;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeTest {
+
+    @Test
+    void testCompositeHierarchyOfEmployees() {
+
+        final Employee boss = new Employee("John","boss", 30000);
+        final Employee teamLead = new Employee("Ivan","teamLead", 30000);
+
+        boss.getSubordinates().add(teamLead);
+
+        final Employee dev1 = new Employee("Asen","dev", 30000);
+        final Employee dev2 = new Employee("Peter","dev", 30000);
+
+        teamLead.getSubordinates().addAll(Arrays.asList(dev1,dev2));
+
+        assertTrue(boss.getSubordinates().size() == 1 && teamLead.getSubordinates().size() == 2);
+    }
+}
