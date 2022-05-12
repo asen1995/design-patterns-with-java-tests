b52654acc7af494d0542f810cae409ed8463ef4f
DESKTOP-K04DLRD\asenn <asen.nikolaev95@abv.bg>
2022-05-12 17:25:40 +0300

    format

--- src/test/java/design/pattern/structural/composite/EmployeeTest.java
+++ src/test/java/design/pattern/structural/composite/EmployeeTest.java
@@ -8,19 +8,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeTest {
 
-    @Test
-    void testCompositeHierarchyOfEmployees() {
+	@Test
+	void testCompositeHierarchyOfEmployees() {
 
-        final Employee boss = new Employee("John","boss", 30000);
-        final Employee teamLead = new Employee("Ivan","teamLead", 30000);
+		final Employee boss = new Employee("John", "boss", 30000);
+		final Employee teamLead = new Employee("Ivan", "teamLead", 30000);
 
-        boss.getSubordinates().add(teamLead);
+		boss.getSubordinates().add(teamLead);
 
-        final Employee dev1 = new Employee("Asen","dev", 30000);
-        final Employee dev2 = new Employee("Peter","dev", 30000);
+		final Employee dev1 = new Employee("Asen", "dev", 30000);
+		final Employee dev2 = new Employee("Peter", "dev", 30000);
 
-        teamLead.getSubordinates().addAll(Arrays.asList(dev1,dev2));
+		teamLead.getSubordinates().addAll(Arrays.asList(dev1, dev2));
 
-        assertTrue(boss.getSubordinates().size() == 1 && teamLead.getSubordinates().size() == 2);
-    }
+		assertTrue(boss.getSubordinates().size() == 1 && teamLead.getSubordinates().size() == 2);
+	}
 }
